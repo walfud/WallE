52711e1c28cff27ef132223b4d414881b1755ae9
walfud <hi@walfud.com>
2017-05-16 20:23:11 +0700

    `ObjectUtils#isEqual` support Collection

--- app/src/main/java/com/walfud/walledemo/MainActivity.java
+++ app/src/main/java/com/walfud/walledemo/MainActivity.java
@@ -6,6 +6,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.walfud.walle.WallE;
+import com.walfud.walle.collection.CollectionUtils;
+import com.walfud.walle.lang.ObjectUtils;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
     public static final String TAG = "MainActivity";
@@ -20,5 +24,43 @@ public class MainActivity extends Activity {
         mBtn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "1", Toast.LENGTH_SHORT).show());
 
         WallE.initialize(getApplicationContext());
+
+        List<Foo> fooList = CollectionUtils.newArrayList(
+                new Foo(1, "1"),
+                new Foo(2, "2"),
+                new Foo(3, "3"),
+                new Foo(4, "4")
+        );
+
+        List<Bar> barList = CollectionUtils.newArrayList(
+                new Bar(1, "1", "other 1"),
+                new Bar(2, "2", "other 2"),
+                new Bar(3, "3", "other 3")
+//                new Bar(4, "4", null)
+        );
+
+        boolean z = ObjectUtils.isEqual(fooList, barList, (a, b) -> a.i - b.ii);
+    }
+
+    public static class Foo {
+        public int i;
+        public String s;
+
+        public Foo(int i, String s) {
+            this.i = i;
+            this.s = s;
+        }
+    }
+
+    public static class Bar {
+        public int ii;
+        public String ss;
+        public String others;
+
+        public Bar(int ii, String ss, String others) {
+            this.ii = ii;
+            this.ss = ss;
+            this.others = others;
+        }
     }
 }
--- library/src/main/java/com/walfud/walle/lang/ObjectUtils.java
+++ library/src/main/java/com/walfud/walle/lang/ObjectUtils.java
@@ -2,9 +2,12 @@ package com.walfud.walle.lang;
 
 import android.text.TextUtils;
 
+import com.walfud.walle.algorithm.Comparator2;
 import com.walfud.walle.collection.CollectionUtils;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by walfud on 2015/12/18.
@@ -29,6 +32,27 @@ public class ObjectUtils {
         }
 
         return true;
+    }
+
+    public static <X, Y> boolean isEqual(Collection<X> xs, Collection<Y> ys, Comparator2<X, Y> comparator2) {
+        if (CollectionUtils.isEmpty(xs) && CollectionUtils.isEmpty(ys)) {
+            return true;
+        } else if (CollectionUtils.isEmpty(xs) && !CollectionUtils.isEmpty(ys)) {
+            return false;
+        } else if (!CollectionUtils.isEmpty(xs) && CollectionUtils.isEmpty(ys)) {
+            return false;
+        } else {
+            Iterator<X> xIter = xs.iterator();
+            Iterator<Y> yIter = ys.iterator();
+            while (xIter.hasNext() && yIter.hasNext()) {
+                X x = xIter.next();
+                Y y = yIter.next();
+                if (comparator2.compareTo(x, y) != 0) {
+                    return false;
+                }
+            }
+            return !xIter.hasNext() && !yIter.hasNext();
+        }
     }
 
     /**

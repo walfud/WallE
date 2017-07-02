d48f24acf7522bfeec09463b723424c1f4748556
walfud <hi@walfud.com>
2017-07-02 19:43:34 +0800

    extensions

---
+++ app/src/main/java/com/walfud/walledemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.walfud.walledemo
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import com.walfud.walle.md5
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener { Toast.makeText(this@MainActivity, "a".md5(), Toast.LENGTH_SHORT).show() }
+    }
+}

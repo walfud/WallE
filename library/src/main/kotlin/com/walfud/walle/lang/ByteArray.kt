8b52dee06757366fe73283963d1cfa4b46cd1767
walfud <hi@walfud.com>
2017-07-13 22:08:37 +0800

    +Byte.base64, Byte.sha1

--- library/src/main/kotlin/com/walfud/walle/lang/ByteArray.kt
+++ library/src/main/kotlin/com/walfud/walle/lang/ByteArray.kt
@@ -1,5 +1,6 @@
 package com.walfud.walle.lang
 
+import android.util.Base64
 import java.security.MessageDigest
 
 /**
@@ -8,4 +9,9 @@ import java.security.MessageDigest
 
 fun ByteArray.string() = map { it.string() }.joinToString("")
 
+fun ByteArray.base64() = String(Base64.encode(this, Base64.DEFAULT))
+
 fun ByteArray.md5() = MessageDigest.getInstance("MD5").run { digest(this@md5).string() }
+fun ByteArray.sha1() = MessageDigest.getInstance("SHA-1").run { digest(this@sha1).string() }
+fun ByteArray.sha256() = MessageDigest.getInstance("SHA-256").run { digest(this@sha256).string() }
+fun ByteArray.sha512() = MessageDigest.getInstance("SHA-512").run { digest(this@sha512).string() }
--- library/src/main/kotlin/com/walfud/walle/lang/String.kt
+++ library/src/main/kotlin/com/walfud/walle/lang/String.kt
@@ -1,7 +1,14 @@
 package com.walfud.walle.lang
 
+import android.util.Base64
+
 /**
  * Created by walfud on 2017/7/9.
  */
 
+fun String.fromBase64() = Base64.decode(this, Base64.DEFAULT)
+
 fun String.md5() = toByteArray().md5()
+fun String.sha1() = toByteArray().sha1()
+fun String.sha256() = toByteArray().sha256()
+fun String.sha512() = toByteArray().sha512()
